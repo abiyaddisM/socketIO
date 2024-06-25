4f8ce8054c8cb6dd68a7e3963ce590181823390f
abiyaddisM <130771857+abiyaddisM@users.noreply.github.com>
2024-06-25 22:03:54 +0300

    final test

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const express = require('express');
-const http = require('http').Server(express);
+const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors'); // Import the cors package
 
 const app = express();
-const io = require('socket.io')(http,{
-    cors:{origin:"*"}
+const httpServer = http.createServer(app); // Create an HTTP server instance
+const io = socketIo(httpServer, {
+    cors: {
+        origin: '*', // Allow all origins for testing purposes
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['Content-Type'],
+        credentials: true
+    }
 });
 
+app.use(cors()); // Use the cors middleware
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('A user connected');
 
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
-        console.log(msg)
-
+        console.log(msg);
     });
 
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('User disconnected');
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
